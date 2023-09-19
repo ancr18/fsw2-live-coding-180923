d7820e267ba151b327e68321db3a645ac909416e
Ardiyansyah R <arsk9797@gmail.com>
2023-09-19 23:09:30 +0800

    live coding 18 sept 2023. buat tampilan dinamis

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const http = require('http')
 const path = require('path')
 const url = require('url')
+
 // menggunakan synchronous
 // const textIn = fs.readFileSync('txt/read-this.txt', 'utf-8')
 // console.log(textIn)
@@ -58,26 +59,52 @@ const url = require('url')
 // })
 
 
-// SERVER dengan HTTP
+// SERVER dengan 
+
+const replaceTemplate = (template, product) => {
+    // /{nama replace}/g = adalah sebuah regex. g untuk global atau kseluruhan
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName)
+    output = output.replace(/{%IMAGE%}/g, product.image)
+    output = output.replace(/{%QUANTITY%}/g, product.quantity)
+    output = output.replace(/{%PRICE%}/g, product.price)
+    output = output.replace(/{%DESCRIPTION%}/g, product.description)
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+    output = output.replace(/{%FROM%}/g, product.from)
+    output = output.replace(/{%ID%}/g, product.id)
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+
+    return output
+}
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+    const {pathname: pathName, query} = url.parse(req.url, true);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf8');
+const overViewPage = fs.readFileSync(`${__dirname}/templates/overview.html`, 'utf8');
+const productTemplate = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf8');
+const productCardTemplate = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf8');
+const dataObj = JSON.parse(data)
 
     // membuat url /hello
     if(pathName === '/hello'){
         res.end('ini hello ke fsw 2')
-        console.log(`${__dirname}`);
     } 
     
     // membuat url /product
     else if(pathName === '/product'){
-        res.end(JSON.stringify({
-            data : 'Halo Bang ini product'
-        }))
+        res.writeHead(200, {
+            'Content-Type' : 'text/html'
+        })
+
+        const product = dataObj[query.id]
+        const output = replaceTemplate(productTemplate, product)
+        res.end(output)
+
+
     } 
 
     // membuat url /api
     else if(pathName === '/api'){
-        const data = fs.readFileSync(`${__dirname}/dev-data/data.json`);
+        
         res.writeHead(200,{
             'Content-type' : 'application/json'
         })
@@ -86,11 +113,14 @@ const server = http.createServer((req, res) => {
     
     // membuat url /overview
     else if(pathName === '/overview'){
-        const overViewPage = fs.readFileSync(`${__dirname}/templates/overview.html`);
+        
         res.writeHead(200,{
             'Content-type' : 'text/html'
         })  
-        res.end(overViewPage)
+
+        const productCardsHtml = dataObj.map(el => replaceTemplate(productCardTemplate, el))
+        const output = overViewPage.replace('{%PRODUCT_CARDS%}', productCardsHtml)
+        res.end(output)
     }
 
     // membuat jika tidak ada url nya
